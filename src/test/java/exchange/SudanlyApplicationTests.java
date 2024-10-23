52e5398ccb5f6941473a2fadfff72d789d974dbd
unknown <elbashier.mohd@gmail.com>
2024-10-23 16:05:23 +0200

    initial

---
+++ src/test/java/exchange/SudanlyApplicationTests.java
@@ -0,0 +1,13 @@
+package exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudanlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
